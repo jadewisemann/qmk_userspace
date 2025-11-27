bec263e692d685ade79452d0d3e65b6a9006568a
jadewisemann <jadewisemann@gmail.com>
2025-11-27 12:19:14 +0900

    test

--- keyboards/lp36/keymaps/mine/keymap.c
+++ keyboards/lp36/keymaps/mine/keymap.c
@@ -129,6 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // **  COMBO ** //
 // COMBO_X(COMBO_NAME, OUTPUT_KEY, INPUT_KEY_1, INPUT_KEY_2, ...)
+#define COMBO_LIST \
     COMBO_X(CB_ENTER,    KC_ENT,        KC_K, KC_L) \
     COMBO_X(CB_ESC,      KC_ESC,        KC_W, KC_E) \
     COMBO_X(CB_CAPSWORD, CW_TOGG,       LSFT_T(KC_Z), RSFT_T(KC_DOT)) \
