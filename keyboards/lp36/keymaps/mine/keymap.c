045485d323e8e4a318be872e9a0f7779c032f5e8
jadewisemann <jadewisemann@gmail.com>
2025-11-27 12:24:01 +0900

    test

--- keyboards/lp36/keymaps/mine/keymap.c
+++ keyboards/lp36/keymaps/mine/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-// **  LAYER & VARIABLE ** //
+// ** LAYER & VARIABLE ** //
 enum layers {
     _BASE = 0,
     _SYM,
@@ -29,7 +29,12 @@ enum custom_keycodes {
     SMART_VSC_M_U  // Up
 };
 
-// **  CHORDAL HOLD HAND DEFINE ** //
+// ** TAP DANCE ENUM (추가됨) ** //
+enum {
+    TD_LANG_FUNC = 0,
+};
+
+// ** CHORDAL HOLD HAND DEFINE ** //
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
     'L', 'L', 'L', 'L', 'L',    'R', 'R', 'R', 'R', 'R',
     'L', 'L', 'L', 'L', 'L',    'R', 'R', 'R', 'R', 'R',
@@ -45,23 +50,15 @@ bool get_chordal_hold(
     uint16_t other_keycode,
     keyrecord_t* other_record
 ) {
-    // 1. 첫 번째 키(Mod-Tap)가 눌린 후 얼마나 지났는지 확인
-    // (other_record->event.time - tap_hold_record->event.time 과 같은 의미)
     uint16_t elapsed = TIMER_DIFF_16(other_record->event.time, tap_hold_record->event.time);
 
-    // 2. "아슬아슬하게(매우 빠르게)" 눌렸을 때만 엄격하게 검사
     if (elapsed < CHORDAL_TERM) {
-        // [엄격 모드]
-        // 반대 손 규칙을 따름 (같은 손이면 Tap, 다른 손이면 Hold)
         return get_chordal_hold_default(tap_hold_record, other_record);
     }
-
-    // 3. 시간이 충분히 지났다면? (여유 있는 입력)
-    // [관대 모드]
-    // 손 위치 상관없이 무조건 Hold(쉬프트 등)로 인정
     return true;
 }
-// **  KEYMAP ** //
+
+// ** KEYMAP ** //
 #define KC_L_THM LGUI_T(KC_ESC) 
 #define KC_R_THM KC_DEL
 #define MAC_LANG KC_F24
@@ -72,7 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,         KC_E,        KC_R,        KC_T,   KC_Y,        KC_U,        KC_I,        KC_O,        KC_SLSH,
         KC_A,         KC_S,         KC_D,        KC_F,        KC_G,   KC_H,        KC_J,        KC_K,        KC_L,        KC_P,
         LSFT_T(KC_Z), LALT_T(KC_X), KC_C,        KC_V,        KC_B,   KC_B,        KC_N,        KC_M,        RALT_T(KC_COMM), RSFT_T(KC_DOT),
-        KC_L_THM,     LT(_SYM, KC_SPC), LCTL_T(KC_TAB), LT(_FUNC, KC_LNG1), LT(_NAV, KC_BSPC),  KC_R_THM
+        // ▼ 여기 TD(TD_LANG_FUNC)로 변경됨!
+        KC_L_THM,     LT(_SYM, KC_SPC), LCTL_T(KC_TAB), TD(TD_LANG_FUNC), LT(_NAV, KC_BSPC),  KC_R_THM
     ),
 
     /* #1: SYMBOL */
@@ -125,27 +123,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-// **  TAP DANCE ** //
+// ** TAP DANCE LOGIC (구현됨) ** //
+// 1. 탭/홀드 동작 정의
+void dance_lang_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        if (!state->pressed) { 
+            // 짧게 눌렀을 때: Ctrl + Space (한영전환)
+            tap_code16(LCTL(KC_SPC)); 
+        } else {
+            // 길게 눌렀을 때: _FUNC 레이어 켜기
+            layer_on(_FUNC);
+        }
+    } 
+}
 
-// **  COMBO ** //
+// 2. 키 뗐을 때 리셋 정의
+void dance_lang_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        // 레이어 끄기
+        layer_off(_FUNC);
+    }
+}
+
+// 3. 액션 등록
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LANG_FUNC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lang_finished, dance_lang_reset)
+};
+
+
+// ** COMBO (에러 수정됨: #define 추가) ** //
 // COMBO_X(COMBO_NAME, OUTPUT_KEY, INPUT_KEY_1, INPUT_KEY_2, ...)
 #define COMBO_LIST \
     COMBO_X(CB_ENTER,    KC_ENT,        KC_K, KC_L) \
     COMBO_X(CB_ESC,      KC_ESC,        KC_W, KC_E) \
     COMBO_X(CB_CAPSWORD, CW_TOGG,       LSFT_T(KC_Z), RSFT_T(KC_DOT)) \
-    COMBO_X(CB_SETTINGS, MO(_SETTINGS), LCTL_T(KC_TAB), LT(_FUNC, KC_LNG1)) \
-    \
-    /* THUMB */\
-    /* COMBO_X(CB_L_THUMB,  KC_L_THM,      LT(_SYM, KC_SPC), LCTL_T(KC_TAB)) \ */\
-    /* COMBO_X(CB_R_THUMB,  KC_R_THM,      LT(_FUNC, KC_LNG1), LT(_NAV, KC_BSPC)) \ */\
-    \
-    /* BOOTLOADER */\
+    COMBO_X(CB_SETTINGS, MO(_SETTINGS), LCTL_T(KC_TAB), TD(TD_LANG_FUNC)) \
     COMBO_X(CB_BOOT_L,   QK_BOOT,       KC_Q, KC_A, LSFT_T(KC_Z)) \
     COMBO_X(CB_BOOT_R,   QK_BOOT,       KC_SLSH, KC_P, RSFT_T(KC_DOT)) \
-    /* MAC_MODE_TOGGLE */\
     COMBO_X(CB_MAC_TOG,  OS_TOGGLE,     KC_Q, KC_W, KC_E)
 
-// MACRO_ENGINE, DON'T NEED TO MODIFY
+// 주의: 위 콤보 리스트에서 TD(TD_LANG_FUNC)를 콤보 입력키로 사용하면 
+// 타이밍 이슈가 생길 수 있습니다. (보통은 단순 키코드를 권장합니다)
+// 일단 기존 코드 살려서 TD(TD_LANG_FUNC)로 바꿔두었습니다.
+
+// MACRO_ENGINE
 enum combos {
     #define COMBO_X(name, result, ...) name,
     COMBO_LIST
@@ -161,7 +182,6 @@ combo_t key_combos[] = {
     COMBO_LIST
     #undef COMBO_X
 };
-// == END (MACRO_ENGINE) == //
 
 
 // ** PER-KEY PERMISSIVE HOLD ** //
@@ -172,13 +192,13 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
                 return true; 
             }
             return false;
-
+        // TD는 permissive hold 설정이 따로 필요 없습니다 (FN_ADVANCED에서 처리됨)
         default:
             return false; 
     }
 }
 
-// **  MACRO & LOGICS & SNATCH ** //
+// ** MACRO & LOGICS & SNATCH ** //
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
@@ -191,71 +211,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
                 return false;
             
-            // VSC > FOCUS > L, D, R, U
+            // VSC Smart Macros
             case SMART_VSC_F_L:
-                if (is_mac_mode) {
-                    tap_code16(LGUI(KC_K));    
-                    tap_code16(LGUI(KC_LEFT)); 
-                } else {
-                    tap_code16(LCTL(KC_K));   
-                    tap_code16(LCTL(KC_LEFT));
-                }
+                if (is_mac_mode) { tap_code16(LGUI(KC_K)); tap_code16(LGUI(KC_LEFT)); } 
+                else             { tap_code16(LCTL(KC_K)); tap_code16(LCTL(KC_LEFT)); }
                 return false;
-
             case SMART_VSC_F_D:
-                if (is_mac_mode) {
-                    tap_code16(LGUI(KC_K));
-                    tap_code16(LGUI(KC_DOWN));
-                } else {
-                    tap_code16(LCTL(KC_K));
-                    tap_code16(LCTL(KC_DOWN));
-                }
+                if (is_mac_mode) { tap_code16(LGUI(KC_K)); tap_code16(LGUI(KC_DOWN)); } 
+                else             { tap_code16(LCTL(KC_K)); tap_code16(LCTL(KC_DOWN)); }
+                return false;
+            case SMART_VSC_F_R: 
+                if (is_mac_mode) { tap_code16(LGUI(KC_K)); tap_code16(LGUI(KC_RIGHT)); } 
+                else             { tap_code16(LCTL(KC_K)); tap_code16(LCTL(KC_RIGHT)); }
+                return false;
+            case SMART_VSC_F_U: 
+                if (is_mac_mode) { tap_code16(LGUI(KC_K)); tap_code16(LGUI(KC_UP)); } 
+                else             { tap_code16(LCTL(KC_K)); tap_code16(LCTL(KC_UP)); }
                 return false;
 
-            case SMART_VSC_F_R: // Focus Right
-                if (is_mac_mode) {
-                    tap_code16(LGUI(KC_K));
-                    tap_code16(LGUI(KC_RIGHT));
-                } else {
-                    tap_code16(LCTL(KC_K));
-                    tap_code16(LCTL(KC_RIGHT));
-                }
-                return false;
-
-            case SMART_VSC_F_U: // Focus Up
-                if (is_mac_mode) {
-                    tap_code16(LGUI(KC_K));
-                    tap_code16(LGUI(KC_UP));
-                } else {
-                    tap_code16(LCTL(KC_K));
-                    tap_code16(LCTL(KC_UP));
-                }
-                return false;
-
-            // VSC > MOVE > L, D, R, U
             case SMART_VSC_M_L:
-                if (is_mac_mode) tap_code16(LGUI(KC_K)); 
-                else             tap_code16(LCTL(KC_K));
-                tap_code(KC_LEFT);
-                return false;
-
+                if (is_mac_mode) tap_code16(LGUI(KC_K)); else tap_code16(LCTL(KC_K));
+                tap_code(KC_LEFT); return false;
             case SMART_VSC_M_D:
-                if (is_mac_mode) tap_code16(LGUI(KC_K)); 
-                else             tap_code16(LCTL(KC_K));
-                tap_code(KC_DOWN);
-                return false;
-
+                if (is_mac_mode) tap_code16(LGUI(KC_K)); else tap_code16(LCTL(KC_K));
+                tap_code(KC_DOWN); return false;
             case SMART_VSC_M_R:
-                if (is_mac_mode) tap_code16(LGUI(KC_K)); 
-                else             tap_code16(LCTL(KC_K));
-                tap_code(KC_RIGHT);
-                return false;
-
+                if (is_mac_mode) tap_code16(LGUI(KC_K)); else tap_code16(LCTL(KC_K));
+                tap_code(KC_RIGHT); return false;
             case SMART_VSC_M_U:
-                if (is_mac_mode) tap_code16(LGUI(KC_K)); 
-                else             tap_code16(LCTL(KC_K));
-                tap_code(KC_UP);
-                return false;
+                if (is_mac_mode) tap_code16(LGUI(KC_K)); else tap_code16(LCTL(KC_K));
+                tap_code(KC_UP); return false;
         }
     }
     return true;
